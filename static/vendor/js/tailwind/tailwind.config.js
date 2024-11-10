3aba51bd04565d64088389bdec0f701ba55c0cf5
Anthony Terán <aterana3@unemi.edu.ec>
2024-11-10 04:05:22 -0500

    Primeros diseños

---
+++ static/vendor/js/tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+		extend: {
+			colors: {
+				'primary': {
+					DEFAULT: '#3a9abf',
+					light: '#00b4c7',
+				}
+			},
+			backgroundImage: {
+				'login': "url('/authentication/login-bg.webp')",
+			}
+		},
+	},
+}
